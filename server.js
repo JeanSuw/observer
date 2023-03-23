8a14aeb0c7715a18fb7839eb9c2a6567540110c9
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-03-23 01:27:09 -0700

    Added more codes in server.js

--- server.js
+++ server.js
@@ -2,3 +2,34 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 
+const questions = [
+    {
+    type: 'list',
+    message: ``,
+    choices: ['something', 'anotherthing'],
+    default: ''
+    },
+];
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'test'
+});
+
+
+inquirer.prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+      console.log("");
+    } else {
+      // Something else went wrong
+      console.log("One of the method is not working. Check one of them");
+    }
+    });
