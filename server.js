38412b0aaa21859d0f8be0f397687abf0405a840
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-03-26 20:01:06 -0700

    Added main menu

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 
-const questions = [
+const menu = [
     {
     type: 'list',
-    message: ``,
-    choices: ['something', 'anotherthing'],
-    default: ''
+    message: `Hi! Wecome to Observer. What would you like to do today?`,
+    choices: ['View All Employee', 'Update Employee Role', 'View All Roles', 'Add Roles', 'View All Department', 'Add Department', 'quit'],
+    default: 'Move up and down using error to view more'
     },
 ];
 
