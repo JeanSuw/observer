412df0acba5f0a48feebe82e77a5bce456465eb0
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-03-27 18:31:23 -0700

    Added more data to seeds.sql relating to roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,11 @@ VALUES ("Sales"),
         ("Engineering"),
         ("Finance"),
         ("Legal");
+
+INSERT INTO role(title,salary,department_id)
+VALUES  ('Sales manager', 95000, 1),
+        ('SW Engineer', 108781, 2),
+        ('Accountant', 90000, 3),
+        ('Lawyer', 80000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
